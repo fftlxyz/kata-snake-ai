3660a88d3b116a22a43fc38f0b7a2c7a6ef92294
fftl.xyz <fly2best@gmail.com>
2025-03-12 09:46:57 +0800

    增加mac、linux安装脚本及相关pip依赖说明

---
+++ install_on_linux.sh
@@ -0,0 +1,15 @@
+
+# Python 3.10.12
+python -m venv venv
+source .venv/bin/activate
+
+# install package from requirements-linux.txt
+# or just installed the latest version it should work...
+
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
+
+pip install tensorboard
+
+pip install stable-baselines3  sb3_contrib Gymnasium
+
+pip install pygame
---
+++ install_on_mac.sh
@@ -0,0 +1,15 @@
+
+#Python 3.10.16
+python -m venv venv
+source .venv/bin/activate
+
+# install package from requirements-mac.txt
+# or just installed the latest version it should work...
+
+pip3 install torch torchvision torchaudio
+
+pip install tensorboard
+
+pip install stable-baselines3  sb3_contrib Gymnasium
+
+pip install pygame
